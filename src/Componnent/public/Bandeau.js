32dfe4bd42c7d01c660dce0b180bf507bade5d99
diaise <diaise@hotmail.fr>
2026-05-22 22:41:41 +0200

    initial commit

--- src/Componnent/public/Bandeau.js
+++ src/Componnent/public/Bandeau.js
@@ -6,13 +6,13 @@ function Bandeau(){
     <div id="carouselExample" className="carousel slide">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="./photo7.jpg" className="d-block w-100" alt="..."/>
+          <img src="./dgforma/photo7.jpg" className="d-block w-100" alt="..."/>
         </div>
         <div className="carousel-item">
-          <img src="./photo6.jpg" className="img-fluid" alt="..."/>
+          <img src="./dgforma/photo6.jpg" className="img-fluid" alt="..."/>
         </div>
         <div className="carousel-item">
-          <img src="./photo5.jpg" className="d-block w-100" alt="..."/>
+          <img src="./dgforma/photo5.jpg" className="d-block w-100" alt="..."/>
         </div>
       </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
