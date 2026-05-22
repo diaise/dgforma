0cc350cc8dca8bceef803ef6e87be0a27a9d4836
diaise <diaise@hotmail.fr>
2026-05-22 08:13:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PublicRouter from './Pages/Public/PublicRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
