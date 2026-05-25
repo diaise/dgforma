ca63d34b04360cff7fc89e1287f78ce6b8986a23
diaise <diaise@hotmail.fr>
2026-05-25 10:01:17 +0200

    initial commit

--- src/Componnent/public/Bandeau.js
+++ src/Componnent/public/Bandeau.js
@@ -6,13 +6,13 @@ function Bandeau(){
     <div id="carouselExample" className="carousel slide">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="photo7.jpg" className="d-block w-100" alt="photo"/>
+          <img src="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdlYnxlbnwwfHwwfHx8MA%3D%3D" className="d-block w-100" alt="photo"/>
         </div>
         <div className="carousel-item">
-          <img src="photo6.jpg" className="img-fluid" alt="..."/>
+          <img src="https://images.unsplash.com/photo-1677691820099-a6e8040aa077?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTgyfHxjciVDMyVBOWF0aW9uJTIwZGUlMjBzaXRlcyUyMHdlYnxlbnwwfHwwfHx8MA%3D%3D" className="img-fluid" alt="..."/>
         </div>
         <div className="carousel-item">
-          <img src="photo5.jpg" className="d-block w-100" alt="..."/>
+          <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2l0ZSUyMGludGVybmV0fGVufDB8fDB8fHww" className="d-block w-100" alt="..."/>
         </div>
       </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
--- src/Pages/Public/About.js
+++ src/Pages/Public/About.js
@@ -17,7 +17,7 @@ const About = () =>{
           </p>
         </div>
         <div className="col-md-6">
-         <img src="./photo5.jpg" className="img-fluid" alt="qui somme nous"/>
+         <img src="https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvcm1hdGlvbiUyMGVuJTIwaW5mb3JtYXRpcXVlfGVufDB8fDB8fHww" className="img-fluid" alt="qui sommes nous"/>
         </div>
       </div>
     </div>
