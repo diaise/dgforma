99171e8f156ca792c7a41b90dfb41f119f49ac7e
diaise <diaise@hotmail.fr>
2026-09-13 10:40:02 +0200

    chagement layout

--- src/Pages/Public/Layout.js
+++ src/Pages/Public/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../Componnent/public/Header';
-import Slide from '../../Componnent/public/Slide';
 import Footer from '../../Componnent/public/Footer';
 
 
@@ -9,7 +8,6 @@ const Layout = () => {
     return (
         <div className='Layout'>
             <Header/>
-            <Slide/>
             <Outlet/>
             <Footer/>
         </div>
